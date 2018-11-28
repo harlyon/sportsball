b5f74d1e90146a02858a72448340cd96583f2cb0
jonathanhoy <jonathanhoy5@gmail.com>
2018-11-27 21:13:50 -0500

    cleaned up files, importing API call

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <h1>We like sportz</h1>
+    )
+  }
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './Styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
