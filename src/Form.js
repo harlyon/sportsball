32455a076a918125901a3a81ca314f2fb73ed97d
jonathanhoy <jonathanhoy5@gmail.com>
2018-11-27 22:10:03 -0500

    succesfully calling list of teams of each league

--- src/Form.js
+++ src/Form.js
@@ -5,19 +5,22 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-      league: 4424
+      league: 'mlb'
     }
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('form changing', this.state.league);
+    this.getLeague();
   }
   getLeague = () => {
-    Axios.get(`https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=4424`)
+    Axios.get(`https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=${this.state.league}`, {      
+    }).then((res) => {
+      console.log(this.state.league, res.data.teams);
+    })
   }
   handleChange = (e) => {
     this.setState({
-      [e.target.id]: parseInt(e.target.value)
+      [e.target.id]: e.target.value
     })
   }
   render() {
@@ -26,10 +29,10 @@ class Form extends Component {
         <p>Please select a league.</p>
         <form action="" onSubmit={this.handleSubmit}>
           <select name="league" id="league" placeholder="Please select a league" value={this.state.league} onChange={this.handleChange}>
-            <option value="4424">Major League Baseball</option>
-            <option value="4380">National Hockey League</option>
-            <option value="4391">National Football League</option>
-            <option value="4387">National Basketball Association</option>
+            <option value="mlb">Major League Baseball</option>
+            <option value="nhl">National Hockey League</option>
+            <option value="nfl">National Football League</option>
+            <option value="nba">National Basketball Association</option>
           </select>
           <input type="submit"/>
         </form>
