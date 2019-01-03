93dcab19e18d88e1aafc62a065339d77fc330198
jonathanhoy <jonathanhoy5@gmail.com>
2019-01-03 16:13:49 -0500

    minor style changes

--- src/About.js
+++ src/About.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 import NoFavoriteTeams from './NoFavoriteTeams';
 
 class About extends Component {
@@ -11,7 +10,7 @@ class About extends Component {
         <p className="about__text">Sportsball is an app designed to connect you to your favorite sports teams! Quickly view the upcoming schedules of your favourite teams so you'll never miss a game.</p>
         <h3 className="about__heading">How to Use</h3>
         <p className="about__text">Select the <strong>Leagues</strong> tab to view the list of leagues supported by the app. Choose a league to see all teams in that league. Select a team to have that team added to your list of favourite teams.</p>
-        <p className="about__text">Under the <strong>Schedules</strong> tab you can see the upcoming schedule for each of your favourite teams. Click <i class="fas fa-sync-alt"></i> to refresh team schedules.</p>
+        <p className="about__text">Under the <strong>Schedules</strong> tab you can see the upcoming schedule for each of your favourite teams. Click <i className="fas fa-sync-alt"></i> to refresh team schedules.</p>
         <p className="about__text">You can edit your list of favourite teams in the <strong>My Teams</strong> tab.</p>
         <NoFavoriteTeams />
       </section>
